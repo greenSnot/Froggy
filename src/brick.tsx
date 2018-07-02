3beb1708d71a51dcb18c8f11778a3be9320a4ef5
Patrick <0x00111111@gmail.com>
2018-07-02 22:21:07 +0800

    mouse wheel

--- src/brick.tsx
+++ src/brick.tsx
@@ -1,119 +1,11 @@
 import React from 'react';
 import { DragEvent, MouseEvent, TouchEvent } from 'react';
-
-export type BrickId = string;
-export type BrickDragEvent = {
-  id: BrickId,
-  mouse_global_x: number,
-  mouse_global_y: number,
-};
-export enum BrickOutput {
-  void,
-  string,
-  number,
-  boolean,
-  array,
-  any,
-}
-
-export enum AtomicBrickEnum {
-  atomic_text = 1,
-  atomic_boolean,
-  atomic_dropdown,
-  atomic_input_string,
-  atomic_input_number,
-  atomic_param,
-  atomic_button,
-}
-
-export type RuntimeBrick = {
-  id?: BrickId,
-  type: string,
-  root?: BrickId,
-  is_root?: boolean,
-
-  parts?: Brick[],
-  prev?: BrickId,
-  next?: Brick,
-  output?: BrickOutput,
-  inputs?: Brick[],
-
-  is_static?: boolean, // avoid to be inserting candidate
-};
-
-export type Brick = RuntimeBrick & {
-  ui?: {
-    className?: string,
-    copier?: boolean, // for output container
-    parent?: BrickId, // part's parent or input's parent
-    show_hat?: boolean,
-    is_removing?: boolean,
-    is_ghost?: boolean,
-    offset?: {
-      x: number,
-      y: number,
-    },
-    is_toolbox_brick?: boolean,
-    value?: any, // output value
-    dropdown?: string, // dropdown
-  },
-};
-
-import { gen_id } from './util';
-
-export const get_ancestor = (bricks, brick: Brick) => {
-  let ancestor = brick;
-  while (ancestor.ui.parent) {
-    ancestor = bricks[ancestor.ui.parent];
-  }
-  return ancestor;
-};
-
-export const clone = (brick: Brick, with_ui = true) => {
-  let root;
-  const do_clone = (b: Brick, prev: BrickId, parent = undefined) => {
-    const id = gen_id();
-    root = root || id;
-    const res = {
-      ...b,
-      parts: b.parts ? b.parts.map(i => do_clone(i, undefined, id)) : undefined,
-      inputs: b.inputs ? b.inputs.map(i => do_clone(i, undefined, id)) : undefined,
-      root,
-      prev: b.output ? undefined : prev,
-      id,
-    } as Brick;
-    if (with_ui) {
-      res.ui = {
-        ...b.ui,
-        parent: parent,
-        delegate: b.output ? undefined : parent,
-      };
-      delete res.ui.is_toolbox_brick;
-      delete res.ui.is_removing;
-      delete res.ui.is_ghost;
-    }
-    if (b.next !== undefined) {
-      res.next = b.next === null ? null : do_clone(b.next, id);
-    }
-    return res;
-  };
-  const root_brick = do_clone(brick, undefined);
-  root_brick.root = undefined;
-  root_brick.ui.offset = {
-    x: 0,
-    y: 0,
-  };
-  root_brick.is_root = true;
-  return root_brick as Brick;
-};
-
-export const get_tail = (b: Brick) => {
-  let tail = b;
-  while (tail.next) {
-    tail = tail.next;
-  }
-  return tail;
-};
+import {
+  Brick,
+  BrickOutput,
+  BrickId,
+  BrickDragEvent,
+} from './types';
 
 type Props = {
   brick_id_to_component: {[id: string]: any},
@@ -132,31 +24,6 @@ type Props = {
   id: BrickId,
 };
 type State = {};
-
-export const for_each_brick = (
-  b: Brick,
-  tail: Brick,
-  cb: Function,
-  range: {
-    inputs?: boolean,
-    parts?: boolean,
-    next?: boolean,
-  } = {
-    inputs: true,
-    parts: true,
-    next: true,
-  }) => {
-  const for_each_child_brick = (brick: Brick) => {
-    cb(brick);
-    range.inputs && brick.inputs && brick.inputs.forEach(i => for_each_child_brick(i));
-    range.parts && brick.parts && brick.parts.forEach(i => for_each_child_brick(i));
-    if (tail && brick.id === tail.id) {
-      return;
-    }
-    range.next && brick.next && for_each_child_brick(brick.next);
-  };
-  for_each_child_brick(b);
-};
 
 class BrickComponent extends React.Component<Props, State> {
   refs = null;
---
+++ src/types.ts
@@ -0,0 +1,57 @@
+export type BrickId = string;
+export type BrickDragEvent = {
+  id: BrickId,
+  mouse_global_x: number,
+  mouse_global_y: number,
+};
+export enum BrickOutput {
+  void,
+  string,
+  number,
+  boolean,
+  array,
+  any,
+}
+
+export enum AtomicBrickEnum {
+  atomic_text = 1,
+  atomic_boolean,
+  atomic_dropdown,
+  atomic_input_string,
+  atomic_input_number,
+  atomic_param,
+  atomic_button,
+}
+
+export type RuntimeBrick = {
+  id?: BrickId,
+  type: string,
+  root?: BrickId,
+  is_root?: boolean,
+
+  parts?: Brick[],
+  prev?: BrickId,
+  next?: Brick,
+  output?: BrickOutput,
+  inputs?: Brick[],
+
+  is_static?: boolean, // avoid to be inserting candidate
+};
+
+export type Brick = RuntimeBrick & {
+  ui?: {
+    className?: string,
+    copier?: boolean, // for output container
+    parent?: BrickId, // part's parent or input's parent
+    show_hat?: boolean,
+    is_removing?: boolean,
+    is_ghost?: boolean,
+    offset?: {
+      x: number,
+      y: number,
+    },
+    is_toolbox_brick?: boolean,
+    value?: any, // output value
+    dropdown?: string, // dropdown
+  },
+};
